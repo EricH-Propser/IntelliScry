c070a8eb2e5c77e0de12b2e99a459b79cec561ad
Eric <eheitmuller@gmail.com>
2017-12-04 21:12:57 -0800

    adding test template

---
+++ src/test/java/com/martyworm/entites/minion/MinionTests.java
@@ -0,0 +1,22 @@
+package com.martyworm.entites.minion;
+
+import org.junit.Test;
+
+public class MinionTests {
+
+    @Test
+    public void shouldMoveToAValidTileNextToIt(){
+
+        //because movement is based on a tile rather than x/y position
+
+        //first we must set up a simple map w/ tiles (i suggest 2 tiles)
+
+        //create a minion
+
+        //put the minion on the left tile
+
+        //send the move command
+
+        //validate the minion is on the correct tile
+    }
+}
